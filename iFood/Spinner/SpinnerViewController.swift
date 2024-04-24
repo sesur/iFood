678d44428a992a3b0e0e677a2fa1b9a1e167491c
sesur <sergiusuru@ymail.com>
2024-04-24 14:24:35 +0300

    r - Improve project hierarchy

---
+++ iFood/Spinner/SpinnerViewController.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+class SpinnerViewController: UIViewController {
+    
+    lazy var activityIndicator : UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView(style: .large)
+        indicator.color = .lightGray
+        return indicator
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSpinner()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        activityIndicator.startAnimating()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) { [weak self] in
+            self?.activityIndicator.startAnimating()
+        }
+    }
+    
+    func addSpinner() {
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(activityIndicator)
+        
+        // Center our spinner both horizontally & vertically
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
