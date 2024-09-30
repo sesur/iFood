4dbe305632ac887671181605e207c2488d000d29
Sergiu Suru <sergiu.suru@macys.com>
2024-09-30 08:47:50 +0300

    T - Add Unit Test target

---
+++ iFoodTests/iFoodTests.swift
@@ -0,0 +1,17 @@
+//
+//  iFoodTests.swift
+//  iFoodTests
+//
+//  Created by Sergiu Suru on 30.09.2024.
+//  Copyright © 2024 Sergiu. All rights reserved.
+//
+
+import Testing
+
+struct iFoodTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
